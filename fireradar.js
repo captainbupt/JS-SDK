f281145a7a920242f3cde5f27b6f993fbda88a62
Weizhou He <285173036@qq.com>
2018-01-03 16:00:51 +0800

    Complete the first version

--- fireradar.js
+++ fireradar.js
@@ -1,4 +1,4 @@
-var fireradarhost1 = "http://127.0.0.1";
+var fireradarhost = "http://101.201.253.36";
 
 function getOrderMapString(object) {
     if ($.type(object) === "object") {
@@ -28,10 +28,14 @@ function getToken(encryption, content) {
     var sb = "";
     sb += contentStr;
     sb += encryption;
-    return $.md5(sb);
+    return hex_md5(sb);
 }
 
-function FireRadar1(param) {
+function sendPage(firerdar) {
+    firerdar.sendPage();
+}
+
+function FireRadar(param) {
     var default_param = {
         domain: document.domain,
         encryption_url: '/encryption',         // Ajax请求uptoken的Url，强烈建议设置（服务端提供）
@@ -60,10 +64,12 @@ function FireRadar1(param) {
         })
     }
     this.param = new_param;
+    this.pages = {};
+    window.setInterval(sendPage, 5000, this);
 }
 
-FireRadar1.prototype = {
-    constructor: FireRadar1,
+FireRadar.prototype = {
+    constructor: FireRadar,
     getFingerPrint: function (callback) {
         var def = $.Deferred();
         var fp = getStorage("fp");
@@ -84,7 +90,7 @@ FireRadar1.prototype = {
             }
             $.ajax({
                 type: "POST",
-                url: fireradarhost1 + ":8085/device/getFingerprintNew?type=js&appcode=" + param.appcode + "&token=" + getToken(param.encryption, obj),
+                url: fireradarhost + ":8085/device/getFingerprintNew?type=js&appcode=" + param.appcode + "&token=" + getToken(param.encryption, obj),
                 data: JSON.stringify(obj),
                 success: function (data) {
                     var fp = data.data.fp;
@@ -104,7 +110,7 @@ FireRadar1.prototype = {
         var def = $.Deferred();
         $.ajax({
             type: "GET",
-            url: fireradarhost1 + ":8085/device/getIP",
+            url: fireradarhost + ":8085/device/getIP",
             success: function (data) {
                 var ip = data.data;
                 if (callback !== null) {
@@ -121,16 +127,8 @@ FireRadar1.prototype = {
         var param = this.param;
         $.when(this.getFingerPrint(null), this.getIP(null)).done(function (fp, ip) {
             var date = new Date();
-            var month = date.getMonth() + 1;
-            var strDate = date.getDate();
-            if (month >= 1 && month <= 9) {
-                month = "0" + month;
-            }
-            if (strDate >= 0 && strDate <= 9) {
-                strDate = "0" + strDate;
-            }
-            var trans_date = date.getFullYear() + "-" + month + "-" + strDate;
-            var trans_time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+            var trans_date = getDateFormat(date);
+            var trans_time = getTimeFormat(date);
             var default_event_param = {
                 organ_key: param.appcode,
                 encryption_key: param.encryption,
@@ -144,7 +142,7 @@ FireRadar1.prototype = {
             var new_param = $.extend(default_event_param, event_param);
             $.ajax({
                 type: "POST",
-                url: fireradarhost1 + ":8082/api/trans",
+                url: fireradarhost + ":8082/api/trans",
                 data: JSON.stringify(new_param),
                 success: function (data) {
                     if (callback !== null) {
@@ -156,8 +154,100 @@ FireRadar1.prototype = {
                 contentType: "application/json"
             });
         })
+    },
+    onloadPage: function (pageid) {
+        this.pages[pageid] = {
+            pageid: pageid,
+            intime: new Date().getTime()
+        };
+    },
+    onunloadPage: function (pageid) {
+        if (this.pages.hasOwnProperty(pageid)) {
+            var page_content = this.pages[pageid];
+            page_content.outtime = new Date().getTime();
+            page_content.period = parseInt((page_content.outtime - page_content.intime) / 1000);
+            var inDate = new Date();
+            inDate.setTime(page_content.intime);
+            page_content.intime = getDateFormat(inDate) + " " + getTimeFormat(inDate);
+
+            var outDate = new Date();
+            outDate.setTime(page_content.outtime);
+            page_content.outtime = getDateFormat(outDate) + " " + getTimeFormat(outDate);
+
+            pushStorage("page", page_content);
+            delete this.pages[pageid];
+        }
+    },
+    onEvent: function (eventid, pageid, attribute) {
+        var date = new Date();
+        var event_content = {
+            eventid: eventid,
+            pageid: pageid,
+            eventtime: getDateFormat(date) + " " + getTimeFormat(date),
+            attribute: JSON.stringify(attribute)
+        };
+        pushStorage("event", event_content);
+    },
+    sendPage: function () {
+        console.log("sending event");
+        var param = this.param;
+        $.when(this.getFingerPrint(null)).done(function (fp) {
+            var data = {};
+            var page = localStorage.getItem("page");
+            if (page !== null) {
+                data["page"] = JSON.parse(page);
+                localStorage.removeItem("page");
+            }
+            var event = localStorage.getItem("event");
+            if (event !== null) {
+                data["event"] = JSON.parse(event);
+                localStorage.removeItem("event");
+            }
+
+            console.log(JSON.stringify(data));
+
+            if (Object.keys(data).length > 0) {
+                $.ajax({
+                    type: "POST",
+                    url: fireradarhost + ":8085/device/setPageInfo?fp=" + encodeURIComponent(fp) + "&type=js&appcode=" + param.appcode + "&token=" + getToken(param.encryption, data),
+                    data: JSON.stringify(data),
+                    success: function (data) {
+                    },
+                    dataType: "json",
+                    contentType: "application/json"
+                });
+            }
+        });
     }
 };
+
+function getTimeFormat(date) {
+    return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+}
+
+function getDateFormat(date) {
+    var month = date.getMonth() + 1;
+    var strDate = date.getDate();
+    if (month >= 1 && month <= 9) {
+        month = "0" + month;
+    }
+    if (strDate >= 0 && strDate <= 9) {
+        strDate = "0" + strDate;
+    }
+    return date.getFullYear() + "-" + month + "-" + strDate;
+}
+
+function pushStorage(key, value) {
+    var data = localStorage.getItem(key);
+    var dataArray;
+    if (data !== null) {
+        dataArray = JSON.parse(data);
+        dataArray.push(value);
+    } else {
+        dataArray = [value];
+    }
+    localStorage.setItem(key, JSON.stringify(dataArray));
+}
 
 function setStorage(key, value, exp) {
     var curTime = new Date().getTime();
